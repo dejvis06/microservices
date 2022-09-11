4802747d5299d7f3ffc2f58051a425b75979239a
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2022-09-11 13:51:39 +0200

    add product composite package and classes to api module

---
+++ api/src/main/java/com/example/api/composite/product/ServiceAddresses.java
@@ -0,0 +1,14 @@
+package com.example.api.composite.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ServiceAddresses {
+
+    private final String cmp;
+    private final String pro;
+    private final String rev;
+    private final String rec;
+}
