94e47a3e2f7ebc6ff9d11df8162bbd940d0dfecb
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2022-09-12 11:25:20 +0200

    add host and port of other services in config file of product-composite service

--- product-composite-service/src/main/java/com/example/api/core/product/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/com/example/api/core/product/ProductCompositeServiceApplication.java
@@ -2,12 +2,18 @@ package com.example.api.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCompositeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompositeServiceApplication.class, args);
+    }
 
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
