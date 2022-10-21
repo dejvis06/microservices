c8766799ebd819d534ee673887eb8fdb4d046c68
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2022-10-21 11:23:41 +0200

    refactor modules

--- product-service/src/main/java/com/example/api/core/product/persistence/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.api.core.product.persistence;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface ProductRepository extends ReactiveCrudRepository<ProductEntity, String> {
-    Mono<ProductEntity> findByProductId(int productId);
-}
